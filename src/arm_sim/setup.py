2a6a65dd20b4a58fdc4107afe5d910a47109da0c
수 <lsu031111@hanyang.ac.kr>
2026-02-04 13:24:18 +0900

    Install MuJoCo robot.xml and meshes with arm_sim

--- src/arm_sim/setup.py
+++ src/arm_sim/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'arm_sim'
@@ -7,25 +9,23 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        # ✅ install MuJoCo assets into share/arm_sim
+        (os.path.join('share', package_name), ['robot.xml']),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='su',
     maintainer_email='lsu031111@hanyang.ac.kr',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='MuJoCo simulation nodes for roboarm.',
+    license='Apache-2.0',
+    extras_require={'test': ['pytest']},
     entry_points={
         'console_scripts': [
             'mujoco_mit_bridge = arm_sim.mujoco_mit_bridge:main',
         ],
-
     },
 )
