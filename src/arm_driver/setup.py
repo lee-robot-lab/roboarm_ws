510f6870bc4dc7ce7427580dd9ca9bd03d1accf7
lee-robot-lab <lsu031111@hanyang.ac.kr>
2026-02-05 16:36:08 +0900

    docs: reorganize README run instructions

--- src/arm_driver/setup.py
+++ src/arm_driver/setup.py
@@ -7,21 +7,16 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='su',
     maintainer_email='lsu031111@hanyang.ac.kr',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='MuJoCo simulation driver publishing/subscribing MIT-mode topics.',
+    license='Apache-2.0',
+    extras_require={'test': ['pytest']},
     entry_points={
         'console_scripts': [
             'mujoco_sim_driver = arm_driver.mujoco_sim_driver:main',
